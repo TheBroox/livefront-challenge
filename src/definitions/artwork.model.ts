5caab75eb4740b759baf3155556876a9c70d881f
Brooks Child <thebroox@gmail.com>
2025-11-30 23:31:04 -0500

    Add comments to definition

--- src/definitions/artwork.model.ts
+++ src/definitions/artwork.model.ts
@@ -1,9 +1,12 @@
 export interface Artwork {
-    artist_display?: string,
-    artist_title: string,
-    date_display?: string,
-    id: number,
-    image_id: number | string,
-    medium_display?: string,
-    title: string
+    // IDs, title and field's ending in "_display" are guaranteed to never 
+    // be empty when coming from AIC. Any other fields have the potential
+    // to be either `null` or and empty string.
+    artist_display?: string,        // Artist or country of origin if known, includes birth and death years if known
+    artist_title: string,           // Artist name if known
+    date_display?: string,          // Start and end year of artwork if known
+    id: number,                     // ID of the artwork in the CIA's database
+    image_id: number | string,      // Alphanumeric that is needed to request images from IAC's API
+    medium_display?: string,        // List of artwork's mediums
+    title: string                   // Title of the artwork if known
 };
